6c0000d409d79887353039c2dd7c2d3617c707bc
Ryan Cavicchioni <ryan@cavi.cc>
2022-08-24 08:52:30 -0500

    Tidy up main

--- cmd/bb/main.go
+++ cmd/bb/main.go
@@ -37,40 +37,9 @@ var (
 )
 
 func main() {
-	var (
-		err error
-	)
-
-	C = bot.NewConfig()
-
-	flag.Bool("debug", false, "enable debug logging")
-	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
-	pflag.Parse()
-	viper.BindPFlags(pflag.CommandLine)
-
-	if viper.GetBool("debug") {
-		log.SetLevel(log.DebugLevel)
-	}
+	setupConfig()
 
 	lib.SeedMathRand()
-
-	viper.SetEnvPrefix("BEEPBOOP")
-	viper.AutomaticEnv()
-	viper.SetConfigName("config")
-	viper.SetConfigType("toml")
-	viper.AddConfigPath(".")
-	err = viper.ReadInConfig()
-	viper.BindEnv("DISCORD_TOKEN")
-	viper.BindEnv("OPEN_WEATHER_MAP_TOKEN")
-
-	if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
-		log.Fatalf("fatal error config file: %v", err)
-	}
-
-	err = viper.Unmarshal(&C)
-	if err != nil {
-		log.Fatalf("unable to decode into struct: %v", err)
-	}
 
 	if C.DiscordToken == "" {
 		log.Fatalf("Discord token is not set")
@@ -102,4 +71,40 @@ func main() {
 	<-sc
 
 	log.Info("Shutting down")
+}
+
+func setupConfig() {
+	var err error
+
+	C = bot.NewConfig()
+
+	flag.Bool("debug", false, "enable debug logging")
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	pflag.Parse()
+	viper.BindPFlags(pflag.CommandLine)
+
+	if viper.GetBool("debug") {
+		log.SetLevel(log.DebugLevel)
+	}
+
+	viper.SetEnvPrefix("BEEPBOOP")
+	viper.AutomaticEnv()
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("toml")
+	viper.AddConfigPath(".")
+
+	err = viper.ReadInConfig()
+
+	viper.BindEnv("DISCORD_TOKEN")
+	viper.BindEnv("OPEN_WEATHER_MAP_TOKEN")
+
+	if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+		log.Fatalf("fatal error config file: %v", err)
+	}
+
+	err = viper.Unmarshal(&C)
+	if err != nil {
+		log.Fatalf("unable to decode into struct: %v", err)
+	}
 }
